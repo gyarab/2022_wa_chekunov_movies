b5cef8ba110a67f577d3659edb7c54707cf5c97d
Egor Chekunov <egor.chekunov@student.gyarab.cz>
2023-03-07 11:11:15 +0100

    Baaaaa

--- gamdb/films/models.py
+++ gamdb/films/models.py
@@ -1,6 +1,7 @@
+# Create your models here.
 from django.db import models
 
-class Move(models.Model):
+class Movie(models.Model):
     name = models.CharField(max_length=200)
     year = models.IntegerField()
-    description = models.CharField()
+    description = models.CharField(max_length=1000)
