1cef83950e807bff67d88b733206731f33a6ee6b
Egor Chekunov <egor.chekunov@student.gyarab.cz>
2023-03-14 11:17:37 +0100

    serverus

--- gamdb/films/admin.py
+++ gamdb/films/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 from .models import Movie
+from .models import Anime
 # Register your models here.
-admin.site.register(Movie)
+
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ['id','name','year']
+
+class AnimeAdmin(admin.ModelAdmin):
+    list_display = ['id','name','year',"country"]
+
+admin.site.register(Movie,MovieAdmin)
+admin.site.register(Anime, AnimeAdmin)
--- gamdb/films/models.py
+++ gamdb/films/models.py
@@ -5,3 +5,17 @@ class Movie(models.Model):
     name = models.CharField(max_length=200)
     year = models.IntegerField()
     description = models.CharField(max_length=1000)
+    
+    
+    def __str__(self):
+        return f"{self.name} ({self.year})"
+
+class Anime(models.Model):
+    name = models.CharField(max_length=200)
+    year = models.IntegerField()
+    country = models.CharField(max_length=200)
+    raiting = models.IntegerField()
+    sfw = models.BooleanField()
+    weebStatus = models.CharField(max_length=200)
+    description = models.CharField(max_length=1000)
+    
