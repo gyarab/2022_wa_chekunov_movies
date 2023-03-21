7ab07b5516e8a5f07f96104b2994bb78a6511481
Egor Chekunov <egor.chekunov@student.gyarab.cz>
2023-03-21 11:03:38 +0100

    ano

--- gamdb/films/admin.py
+++ gamdb/films/admin.py
@@ -1,13 +1,21 @@
 from django.contrib import admin
 from .models import Movie
 from .models import Anime
+from .models import Director
+from .models import Genre
 # Register your models here.
 
 class MovieAdmin(admin.ModelAdmin):
     list_display = ['id','name','year']
 
+
 class AnimeAdmin(admin.ModelAdmin):
     list_display = ['id','name','year',"country"]
 
+class DirectorAdmin(admin.ModelAdmin):
+    list_display = ['id','name','birth_year']
+
 admin.site.register(Movie,MovieAdmin)
 admin.site.register(Anime, AnimeAdmin)
+admin.site.register(Director)
+admin.site.register(Genre)
--- gamdb/films/models.py
+++ gamdb/films/models.py
@@ -4,9 +4,9 @@ from django.db import models
 class Movie(models.Model):
     name = models.CharField(max_length=200)
     year = models.IntegerField()
-    description = models.CharField(max_length=1000)
-    
-    
+    description = models.CharField(max_length=1000, blank=True)
+    director = models.ForeignKey('Director', blank=True, null=True, on_delete=models.SET_NULL)
+    genres = models.ManyToManyField('Genre',blank=True, null=True)
     def __str__(self):
         return f"{self.name} ({self.year})"
 
@@ -19,3 +19,16 @@ class Anime(models.Model):
     weebStatus = models.CharField(max_length=200)
     description = models.CharField(max_length=1000)
     
+
+class Genre(models.Model):
+    name = models.CharField(max_length=200)
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Director(models.Model):
+    name = models.CharField(max_length=200)
+    birth_year = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.name} ({self.birth_year})"
