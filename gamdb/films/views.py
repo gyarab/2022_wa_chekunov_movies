d6f174dd89a62548487e2a40bd3a6dc9a41d0b0f
Egor Chekunov <egor.chekunov@student.gyarab.cz>
2023-03-28 11:12:59 +0200

    hodina 28.3

--- gamdb/films/views.py
+++ gamdb/films/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import Movie
 # Create your views here.
+def homepage(request):
+    context ={
+        'Movie': Movie.objects.all(),
+        'value': 'Filmy'
+    }
+    return render(request,'main.html',context)
