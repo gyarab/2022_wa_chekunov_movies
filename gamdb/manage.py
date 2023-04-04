3c0004bb65a7977d87cdc845287159a9011e36d7
Egor Chekunov <egor.chekunov@student.gyarab.cz>
2023-04-04 11:22:36 +0200

    comm yaml

--- gamdb/manage.py
+++ gamdb/manage.py
@@ -2,7 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+#$ ./manage.py dumpdata --format yaml films > fixtures/films.yaml 
 
 def main():
     """Run administrative tasks."""
