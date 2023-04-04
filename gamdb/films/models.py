80088022de2bb8b110b2238ac2322624795afb82
egor <egor.chekunov@student.gyarab.cz>
2023-04-04 18:06:25 +0200

    ano

--- gamdb/films/models.py
+++ gamdb/films/models.py
@@ -6,7 +6,7 @@ class Movie(models.Model):
     year = models.IntegerField()
     description = models.CharField(max_length=1000, blank=True)
     director = models.ForeignKey('Director', blank=True, null=True, on_delete=models.SET_NULL)
-    genres = models.ManyToManyField('Genre',blank=True, null=True)
+    genres = models.ManyToManyField('Genre',blank=True,)
     def __str__(self):
         return f"{self.name} ({self.year})"
     def genre_display(self):
